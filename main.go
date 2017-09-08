052fd8f50aa59dc4290111747bb7243510f1d629
David Ndungu <dndungu@zendesk.com>
2017-09-07 17:03:38 -0700

    gray

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 		BackgroundColour string
 		Colour           string
 		Name             string
-	}{BackgroundColour: "#000000", Colour: "#888888", Name: "black"}
+	}{BackgroundColour: "#444444", Colour: "#888888", Name: "grey"}
 
 	t, err := template.ParseFiles("page.tpl")
 
